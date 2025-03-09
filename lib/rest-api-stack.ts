e477a5108b6b7530ef32a2a5015930735dbc40d2
Xinyue <858937756@qq.com>
2025-03-09 23:19:17 +0000

    Add API endpoint for posting a new movie.

--- lib/rest-api-stack.ts
+++ lib/rest-api-stack.ts
@@ -55,6 +55,20 @@ export class RestAPIStack extends cdk.Stack {
         }
         );
         
+        // Lambda to Add Movie
+        const newMovieFn = new lambdanode.NodejsFunction(this, "AddMovieFn", {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_22_X,
+          entry: `${__dirname}/../lambdas/addMovie.ts`,
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+          environment: {
+            TABLE_NAME: moviesTable.tableName,
+            REGION: "eu-west-1",
+          },
+        });
+
+        // Initialize DynamoDB with data
         new custom.AwsCustomResource(this, "moviesddbInitData", {
           onCreate: {
             service: "DynamoDB",
@@ -74,7 +88,8 @@ export class RestAPIStack extends cdk.Stack {
         // Permissions 
         moviesTable.grantReadData(getMovieByIdFn)
         moviesTable.grantReadData(getAllMoviesFn)
-        
+        moviesTable.grantReadWriteData(newMovieFn);
+
         // REST API 
     const api = new apig.RestApi(this, "RestAPI", {
       description: "demo api",
@@ -101,7 +116,12 @@ export class RestAPIStack extends cdk.Stack {
       "GET",
       new apig.LambdaIntegration(getMovieByIdFn, { proxy: true })
     );
-    
+
+    // POST method to add a movie
+    moviesEndpoint.addMethod(
+      "POST",
+      new apig.LambdaIntegration(newMovieFn, { proxy: true })
+    );
       }
     }
     
